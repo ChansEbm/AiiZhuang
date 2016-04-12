84f7d085d8382b9d9102721a51b12185f0db7e1c
Chan'sEbm <chenzibin134@126.com>
2016-04-12 14:14:02 +0800

    12/4

--- app/src/main/java/com/appbaba/iz/ui/fragment/AlbumFragment.java
+++ app/src/main/java/com/appbaba/iz/ui/fragment/AlbumFragment.java
@@ -1,23 +1,55 @@
 package com.appbaba.iz.ui.fragment;
 
+import android.support.annotation.Size;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.AppCompatRadioButton;
 import android.view.View;
+import android.widget.RadioGroup;
 
+import com.appbaba.iz.AlbumLayout;
 import com.appbaba.iz.R;
 import com.appbaba.iz.base.BaseFgm;
+import com.appbaba.iz.entity.Base.BaseBean;
+import com.appbaba.iz.ui.fragment.album.EffectFragment;
+import com.appbaba.iz.ui.fragment.album.ProductFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by ruby on 2016/4/1.
  */
-public class AlbumFragment extends BaseFgm {
+public class AlbumFragment extends BaseFgm implements ViewPager.OnPageChangeListener, RadioGroup
+        .OnCheckedChangeListener {
+    private ViewPager viewPager;
+    private RadioGroup radioGroup;
+    private AppCompatRadioButton rbEffect;
+    private AppCompatRadioButton rbProduct;
+
+    private List<Fragment> fragments = new ArrayList<>();
 
     @Override
     protected void initViews() {
-
+        AlbumLayout albumLayout = (AlbumLayout) viewDataBinding;
+        viewPager = albumLayout.viewPager;
+        radioGroup = albumLayout.radioGroup;
+        rbEffect = albumLayout.rbEffect;
+        rbProduct = albumLayout.rbProduct;
     }
 
     @Override
     protected void initEvents() {
+        fragments.add(new EffectFragment());
+        fragments.add(new ProductFragment());
 
+        viewPager.setAdapter(new Adapter(getChildFragmentManager()));
+        viewPager.addOnPageChangeListener(this);
+        rbEffect.setChecked(true);
+
+        radioGroup.setOnCheckedChangeListener(this);
     }
 
     @Override
@@ -33,5 +65,69 @@ public class AlbumFragment extends BaseFgm {
     @Override
     protected int getContentView() {
         return R.layout.fragment_album;
+    }
+
+    public void ss(@Size(max = 5) List<List<? extends BaseBean>> s) {
+
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        if (positionOffset != 0.0f) {
+            final float cAlpha = 0.5f;
+            boolean pCheck = rbEffect.isChecked() ? rbEffect.isChecked() : rbProduct.isChecked();
+            final float offSet = positionOffset * cAlpha;
+            final float alpha = cAlpha + offSet;
+            if (pCheck) {
+                rbEffect.setAlpha(1 - offSet);
+                rbProduct.setAlpha(alpha);
+            } else {
+                rbProduct.setAlpha(1 - offSet);
+                rbEffect.setAlpha(alpha);
+            }
+        }
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (position == 0) {
+            rbEffect.setChecked(true);
+            rbProduct.setChecked(false);
+        } else if (position == 1) {
+            rbEffect.setChecked(false);
+            rbProduct.setChecked(true);
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        if (checkedId == rbEffect.getId()) {
+            viewPager.setCurrentItem(0, true);
+        } else if (checkedId == rbProduct.getId()) {
+            viewPager.setCurrentItem(1, true);
+        }
+    }
+
+
+    private class Adapter extends FragmentPagerAdapter {
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
     }
 }
---
+++ app/src/main/java/com/appbaba/iz/widget/LoadingView.java
@@ -0,0 +1,84 @@
+package com.appbaba.iz.widget;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.appbaba.iz.R;
+
+/**
+ * Created by Administrator on 2016/4/12.
+ * LoadingView
+ */
+public class LoadingView extends View implements Runnable {
+
+    private Bitmap bmFill;
+    private Bitmap bmEmpty;
+    private int eachInch;
+    private int inch;
+    private int bw;
+    private int bh;
+    private Handler handler = new Handler();
+    private Paint paint;
+
+    public LoadingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        bmFill = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_loading);
+        bmEmpty = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_loading_nor);
+        bw = bmFill.getWidth();
+        bh = bmFill.getHeight();
+        paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setDither(true);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        eachInch = bh / 30;//分成30份
+        inch = eachInch;
+        setMeasuredDimension(bw, bh);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Rect eDst = new Rect(0, 0, bw, bh);
+        canvas.drawBitmap(bmEmpty, null, eDst, paint);
+        Rect src = new Rect(0, bh - inch, bw, bh);
+        Rect dst = new Rect(src);
+        canvas.drawBitmap(bmFill, src, dst, paint);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        handler.post(this);
+    }
+
+    @Override
+    public void run() {
+        inch += eachInch;
+        if (inch >= bh) {
+            inch = bh;
+        }
+        postInvalidate();
+        if (inch >= bh)
+            inch = eachInch;
+        handler.postDelayed(this, 60);//每隔60ms画多1/30
+    }
+
+    public void cancel() {
+        handler.removeCallbacks(this);
+    }
+
+    public void start() {
+        handler.post(this);
+    }
+}
